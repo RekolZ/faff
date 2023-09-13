ac01c22866879a5b7d318e0334c1115ee29fd76f
rotoyouoi <rotoyouoi@yandex.ru>
2023-09-13 13:28:56 +0500

    gomit

---
+++ app/src/main/java/com/example/faff/ui/profile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+//package com.example.faff.ui.profile
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.ViewModelProvider
+//import com.example.faff.databinding.FragmentNotificationsBinding
+//
+//class ProfileFragment : Fragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+//
+//        return root
+//    }
+//}
